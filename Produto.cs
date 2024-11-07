d80e4a8fdd506755954c71f9f29c41040f843d04
Alessandro Luiz Silva <2021202060039@aluno.ifro.edu.br>
2024-11-06 21:17:09 -0400

    primeiro commit

---
+++ Produto.cs
@@ -0,0 +1,20 @@
+﻿public class Produto
+{
+    public int id;
+    public string descricao;
+    public string nome;
+    public double valorCompra;
+    public string categoria;
+    public string unidade;
+    public double lucro;
+    public double precoVenda;
+
+    public double PrecoVenda(double valorCompra, double lucro)
+    {
+        precoVenda = valorCompra + lucro;
+        return precoVenda;
+    }
+
+
+}
+
